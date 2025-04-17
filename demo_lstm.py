7f2c1075efa310d5d2a443ace4fb5524f62f918e
Berend Grandt <beraulndt@gmail.com>
2025-04-17 01:00:41 -0500

    Bumped up those completion length numbers

--- demo_lstm.py
+++ demo_lstm.py
@@ -1,11 +1,12 @@
 import sentencepiece as spm
 import torch
+from Models import MyLSTM
 
 tokenizer = (spm.SentencePieceProcessor())
 tokenizer.LoadFromFile("./sentence-piece/big.model")
-lstm = torch.load("./saved-models/lstm-04-16-2025_11-38am.pth")
+lstm: MyLSTM = torch.load("./saved-models/lstm-04-16-2025_11-38am.pth")
 
 common_prompt = "Do you prefer cats or dogs?"
 custom_prompt = "A long time ago in a galaxy far far away..."
-print(f"\tPrompt: {common_prompt}\n\t{lstm.prompt(common_prompt)}")
-print(f"\tPrompt: {custom_prompt}\n\t{lstm.prompt(custom_prompt)}")
+print(f"\tPrompt: {common_prompt}\n\t{lstm.prompt(common_prompt, max_completion_length=250)}")
+print(f"\tPrompt: {custom_prompt}\n\t{lstm.prompt(custom_prompt, max_completion_length=250)}")
--- demo_rnn.py
+++ demo_rnn.py
@@ -1,11 +1,12 @@
 import sentencepiece as spm
 import torch
+from Models import MyRNN
 
 tokenizer = (spm.SentencePieceProcessor())
 tokenizer.LoadFromFile("./sentence-piece/big.model")
-rnn = torch.load("./saved-models/rnn-04-16-2025_09-54pm")
+rnn: MyRNN = torch.load("./saved-models/rnn-04-16-2025_09-54pm")
 
 common_prompt = "Do you prefer cats or dogs?"
 custom_prompt = "Ralof: Hey, you. You're finally awake."
-print(f"\tPrompt: {common_prompt}\n\t{rnn.prompt(common_prompt)}")
-print(f"\tPrompt: {custom_prompt}\n\t{rnn.prompt(custom_prompt)}")
+print(f"\tPrompt: {common_prompt}\n\t{rnn.prompt(common_prompt, max_completion_length=250)}")
+print(f"\tPrompt: {custom_prompt}\n\t{rnn.prompt(custom_prompt, max_completion_length=250)}")
